d9ba52f6f04efd8100d7c93ab81653a7cf94484f
YannDubs <yanndubois96@gmail.com>
2022-09-26 15:03:26 -0700

    more digging

--- main.py
+++ main.py
@@ -4,24 +4,29 @@ import pytorch_lightning as pl
 from pl_bolts.datamodules import SklearnDataModule
 from sklearn.datasets import load_diabetes
 from pytorch_lightning.plugins.environments import SLURMEnvironment
-import signal
+import numpy as np
+import time
+from pytorch_lightning.accelerators import CUDAAccelerator
+from pytorch_lightning.utilities import device_parser
+from pytorch_lightning.strategies.launchers.multiprocessing import _is_forking_disabled
+import multiprocessing
+import torch
 
 @hydra.main(config_name="sigterm", config_path="config")
 def main_except(cfg):
-    import os
-    del os.environ["SLURM_NTASKS"]
-    del os.environ["SLURM_JOB_NAME"]
 
-    X, y = load_diabetes(return_X_y=True)
-    loaders = SklearnDataModule(X, y)
+    time.sleep(2)
 
-    model = LinearRegression(input_dim=10)
-    #signal.signal(signal.SIGUSR2, signal.SIG_DFL)
-    trainer = pl.Trainer(accelerator="cpu",  # error on cpu and gpu
-                         plugins=[SLURMEnvironment(auto_requeue=False)],
-                         enable_progress_bar=False)
-    trainer.fit(model, train_dataloaders=loaders.train_dataloader(), val_dataloaders=loaders.val_dataloader())
 
+    print("multi", flush=True)
+    with multiprocessing.get_context("fork").Pool(1) as pool:
+        time.sleep(2)
+        print("apply", flush=True)
+        pool.apply(torch.cuda.device_count)
+        time.sleep(2)    
+
+    print('done', flush=True)
+    time.sleep(2)
 
 if __name__ == "__main__":
     main_except()
