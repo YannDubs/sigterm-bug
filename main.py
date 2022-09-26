effc81d5d6d76f18e5e72622404aa2f4b6bede34
YannDubs <yanndubois96@gmail.com>
2022-09-26 13:00:02 -0700

    deactivate SLURM

--- main.py
+++ main.py
@@ -3,16 +3,22 @@ from pl_bolts.models.regression import LinearRegression
 import pytorch_lightning as pl
 from pl_bolts.datamodules import SklearnDataModule
 from sklearn.datasets import load_diabetes
-
+from pytorch_lightning.plugins.environments import SLURMEnvironment
+import signal
 
 @hydra.main(config_name="sigterm", config_path="config")
 def main_except(cfg):
+    import os
+    del os.environ["SLURM_NTASKS"]
+    del os.environ["SLURM_JOB_NAME"]
 
     X, y = load_diabetes(return_X_y=True)
     loaders = SklearnDataModule(X, y)
 
     model = LinearRegression(input_dim=10)
+    #signal.signal(signal.SIGUSR2, signal.SIG_DFL)
     trainer = pl.Trainer(accelerator="cpu",  # error on cpu and gpu
+                         plugins=[SLURMEnvironment(auto_requeue=False)],
                          enable_progress_bar=False)
     trainer.fit(model, train_dataloaders=loaders.train_dataloader(), val_dataloaders=loaders.val_dataloader())
 
